7934e8bef17a0628c69604cd11ae0ae7acd14b55
Maxwell Logan <32340228+logangator2@users.noreply.github.com>
2019-04-26 11:07:55 -0700

    Movement+Broken Camera

---
+++ Test Grapple/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    Camera Camera;
+
+    void Start ()
+    {
+        
+    }
+    
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // if grapple not activated, shoot grapple
+            // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); - unity doc
+        }
+        // print(Input.mousePosition);
+        transform.rotation = Quaternion.Euler(Input.mousePosition);
+    }
+}
---
+++ Test Grapple/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // some movement from source: https://www.mvcode.com/lessons/first-person-camera-and-controller-jamie
+
+    public float walkSpeed;
+
+    Rigidbody rb;
+    Vector3 moveDirection;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Non-Physics steps
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        moveDirection = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        // Physics steps
+        Move();
+    }
+
+    void Move()
+    {
+        // to fix falling slowly
+        Vector3 yVelFix = new Vector3(0, rb.velocity.y, 0);
+        // to move normally
+        rb.velocity = moveDirection * walkSpeed * Time.deltaTime;
+        // adding fixed fall velocity
+        rb.velocity += yVelFix;
+    }
+}
