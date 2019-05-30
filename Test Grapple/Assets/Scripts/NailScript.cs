5bb554178c8089bbcaf071745c38ab31b8ed2448
Andi <andinosler@gmail.com>
2019-05-30 16:32:59 -0700

    Changed Nail Script

--- Test Grapple/Assets/Scripts/NailScript.cs
+++ Test Grapple/Assets/Scripts/NailScript.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class NailScript : MonoBehaviour
 {
-    private float flySpeed = 30f;
-
-    void FixedUpdate()
-    {
-        transform.position += transform.up * Time.deltaTime * flySpeed;
-    }
-
     private void OnCollisionEnter(Collision other)
     {
-        Destroy(GetComponent<Rigidbody>());
+        if(other.gameObject.tag == "Robot")
+        {
+            gameObject.transform.parent = other.transform;
+        }
+
         Destroy(GetComponent<MeshCollider>());
         Destroy(gameObject, 20f);
         Destroy(this);
