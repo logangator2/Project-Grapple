1cbca9927704f88c24dbc9ed27b793e22fef9b60
Maxwell Logan <logangator2@gmail.com>
2019-05-30 19:36:10 -0700

    UI Loading Image

--- Test Grapple/Assets/Scripts/LoadOnClick.cs
+++ Test Grapple/Assets/Scripts/LoadOnClick.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// UI code from Unity tutorial: https://learn.unity.com/tutorial/live-sessions-on-ui#5c7f8528edbc2a002053b4b3
+
 public class LoadOnClick : MonoBehaviour
 {
+    public GameObject LoadingImage;
     public void LoadScene(int level)
     {
+        LoadingImage.SetActive(true);
         SceneManager.LoadScene(level);
     }
 }
