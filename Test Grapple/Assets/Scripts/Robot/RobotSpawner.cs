36cf6a4ad46439b7dc25ac639587f9c06871f5cb
Maxwell Logan <logangator2@gmail.com>
2019-06-07 18:19:18 -0700

    Spawner now spawns every x seconds

--- Test Grapple/Assets/Scripts/Robot/RobotSpawner.cs
+++ Test Grapple/Assets/Scripts/Robot/RobotSpawner.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class RobotSpawner : MonoBehaviour
 {
+    public float spawnDelay;
     public GameObject[] spawners;
     public GameObject SpiderBot;
     void Awake()
+    {
+        foreach (var spawner in spawners)
+        {
+            Instantiate(SpiderBot, spawner.transform);
+        }
+    }
+
+    void Start()
+    {
+        InvokeRepeating("AwaitSpawn", spawnDelay, spawnDelay);
+    }
+
+    void AwaitSpawn()
     {
         foreach (var spawner in spawners)
         {
